da74b716c1625de743596bcbe94a79e3d26aa30d
Alejandro Marin <ingenieroalejandromarin@gmail.com>
2025-03-19 15:40:46 -0500

    Add codefirst in PaymentService

---
+++ PaymentService/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentService.Domain.Entities;
+
+namespace PaymentService.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Payment> Payments { get; set; } // Agrega las entidades necesarias
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Payment>().HasKey(p => p.Id);
+        }
+    }
+}
